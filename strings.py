afd764aea59b6be290c01f26996a14fc2708412e
Yedidya Laufer <yloufer@gmail.com>
2019-07-29 09:56:31 +0300

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,83 @@
+resend_link = "נא לשלוח את הקישור שוב 😕"
+no_result = "לא נמצאה שום תוצאה 😕"
+were_banned = "נחסמת 😈"
+the_song = "השיר "
+too_big = " גדול מדי מכדי להשלח"
+track_not_in_deezer = "מצטער, נראה שהשיר אינו קריא בDeezer :("
+album_not_in_deezer = "האלבום לא נמצא בDeezer :("
+try_inline = "נסה לחפש את השיר במצב שורה, או למצוא את הקישור בDeezer"
+cant_find_track = "מצטער, מסיבה מסוימת לא הצלחתי למצוא את השיר :("
+downloading_from_utube = "השיר אינו קיים בDeezer או ייתכן שהוא אינו קריא, הוא יורד מYouTube..."
+cant_download = "מצטער, אני לא יכול למצוא שיר זה :("
+# about file
+track = "שיר: "
+track_amount = "מספר שירים:"
+artist = "אמן: "
+album = "אלבום: "
+date = "תאריך: "
+creation = "יצירה:"
+label = "תוית: "
+user = "משתמש:"
+genre = "Genre: "
+album_number = "מספר אלבומים:"
+fans_deezer = "אוהדים בDeezer:"
+
+# inline buttons
+top_thirty = "ה-30 הטובים ביותר"
+albums = "אלבומים"
+radio = "רדיו"
+
+
+inval_link = "קישור לא תקין 😔"
+ddos_ass = "If you do this again I will come to your home and I will ddos your ass :)"
+curse = "Fuck you"
+not_found = " לא נמצא 😞"
+was_error = "תקלה 🥴"
+unsupported_link = "מצטערים, הבוט אינו תומך בקישור זה 😵"
+
+seems_flood = "נראה שאתה מנסה להציף את הבוט, עליך לחכות יותר משניה אחת כדי לשלוח הודעה נוספת."
+tries_left = " נסיונות 🙃"
+contact = "אופס :( קרתה תקלה\nאנא צור קשר עם @Yedidya012 כדי להסביר את הבעיה"
+finished = "סיימתי 🥳"
+more_than_twenty = "הקובץ שנשלח גדול מדי, אנא שלח קובץ קטן מ20 MB"
+cant_detect = "מצטער, לא הצלחתי לזהות את השיר מהאודיו 😕, נסה שנית..."
+download = "הורד"
+info = "אודות"
+back = "חזור 🔙"
+get_all = "קבל הכל ⬇️"
+wait = "חכה עד הסוף וחזור על השלב, חשבת שאתה יכול להוריד כמה שירים שתרצה? ;)"
+songs_downloading = "השירים מורדים"
+song_donloading = "השיר מורד"
+
+n_tracks = "מס' שירים:"
+welcome = "ברוכים הבאים ל@Deezer_spotify_bot\nליחצו על '/' כדי לראות את רשימת הפקודות"
+manual = "לחץ על הכפתורים כדי לחפש שירים אלבומים או אמנים.\nנ.ב, זכור שניתן להשתמש בבוט באמצעות הקלדת @ בכל צ'אט, ובחירת Deezer_spotify_bot.\nשימוש מהנה!😌"
+search_by_artist = "חיפוש לפי אמן"
+search_by_album = "חיפוש לפי אלבום"
+global_search = "חיפוש כללי"
+
+changed_to_hebrew = "השפה הוגדה לעברית"
+will_use_your_language = "הבוט ישתמש מעכשיו בשפת אפליקציית הטלגרם שלך"
+select_quality = "בחר איכות הורדה כברירת מחדל"
+
+flac = "FLAC"  # Do NOT TOUCH!
+kb_320 = "MP3_320Kbps"  # Do NOT TOUCH!
+kb_256 = "MP3_256Kbps"  # Do NOT TOUCH!
+kb_128 = "MP3_128Kbps"  # Do NOT TOUCH!
+
+
+will_download_in_quality = "השירים יורדו באיכות "
+best_quality_available = "שירים שלא יוכלו לרדת באיכות שההגדרת, יורדו בריכות הטובה ביותר האפשרית"
+
+
+about = "גרסה: 1.3 Laufer\nשם: @Deezer_spotify_bot\nיוצר: @Yedidya012\nמשתמשים: "
+total_downloads = 'סה"כ הורדות: '
+
+press = "לחץ"
+
+default_language = "he"  # leave in english
+
+inline_kboard_album = "אלבום:"
+inline_kboard_artist = "אמן:"
+inline_kboard_album_en = "album:"
+inline_kboard_artist_en = "artist:"
